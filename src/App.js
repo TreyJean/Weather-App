624bb3939ebe1a1a56b9d2e9173440f4b2fe861a
samleeusmc7 <saml777@yahoo.com>
2023-03-18 09:21:26 -0400

    changes to json and adding in router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+import Home from '../src/components/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      Hello World.
+    <div className="wrapper">
+      <h1>Marine Mammals</h1>
+      <BrowserRouter>
+       <Home />
+
+      </BrowserRouter>
     </div>
   );
 }
